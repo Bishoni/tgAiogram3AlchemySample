c4b1b0b52a5f913e57935f1c6408e4e478044a4b
Bishoni <addim111@yandex.ru>
2025-04-01 18:45:05 +0700

    Убрано необязательное поле

--- app/dto/Models/Admin/SAdminUpdate.py
+++ app/dto/Models/Admin/SAdminUpdate.py
@@ -6,9 +6,8 @@ class SAdminUpdate(_AdminBase):
     Pydantic-схема обновления администратора
 
     Attributes:
-        telegram_id: Телеграмм-айди пользователя
         permission_level: Уровень разрешений администратора
         is_active: Активна ли админка в данный момент
     """
-    telegram_id: int
+    pass
     
